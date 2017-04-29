e6190756b3bd33b06cac53cf77535cacec92f6d6
dylanrodbar <dylanrodbar97@gmail.com>
2017-04-28 23:52:32 -0600

    Se agregó mini navegación
    Incluyendo: dos html y manejo de links en estos

--- appeGov/urls.py
+++ appeGov/urls.py
@@ -2,6 +2,15 @@ from django.conf.urls import url
 
 from . import views
 
+
+app_name = 'appeGov'
 urlpatterns = [
+
+    #/appeGov/
     url(r'^$', views.index, name='index'),
+
+    #/appeGov/numero/
+    url(r'^(?P<pic_id>[0-9]+)/$', views.profilepic, name='profilepic'),
+
+    
 ]
--- appeGov/views.py
+++ appeGov/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
 
 
+from .models import Image, Post, Profilepicture, Stadistic, Tag, Tagsxpost, User, Usertype
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Esta es una aplicación sobre democracia digital")
+    latest_question_list = Profilepicture.objects.all()[:6]
+    template = loader.get_template('appeGov/index.html')
+    context = {
+        'latest_question_list': latest_question_list, #se referencia la variable que usará el .html
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def profilepic(request, pic_id):
+    #try:
+    pic = get_object_or_404(Profilepicture, pk=pic_id)
+        #pic = Profilepicture.objects.get(id=pic_id)
+    template = loader.get_template('appeGov/pic.html')
+    context = {
+        'pic': pic
+    }
+    
+    #except Profilepicture.DoesNotExist:
+    #    raise Http404("Picture does not exist")
+
+    return HttpResponse(template.render(context, request))
